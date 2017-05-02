e7da02c92b0be34dba0683281c35198345985b20
daniclar156 <daniclar156@ed.amdsb.ca>
2017-05-02 13:57:43 -0400

    Add project files.

---
+++ SuperSnakeGame/Form1.cs
@@ -0,0 +1,49 @@
+﻿//Lake
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BrickBreaker.Screens;
+/// <summary>
+/// stefan andrekovic
+/// 
+/// powerups
+///     long paddle
+///     multiball
+///     backup floor
+///     lives
+///     
+/// powerdowns(?)
+///     brick enhancer
+///     
+/// </summary>
+namespace BrickBreaker
+{
+    public partial class Form1 : Form
+    {
+        // add a global value here
+        // add a new comment
+
+        //test comment 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Start the program centred on the Menu Screen
+            MenuScreen ps = new MenuScreen();
+            this.Controls.Add(ps);
+
+            ps.Location = new Point((this.Width - ps.Width) / 2, (this.Height - ps.Height) /2);
+        }
+    }
+}
